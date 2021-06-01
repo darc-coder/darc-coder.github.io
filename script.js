83cd8eba10ba6523036e8f270d845dbc2281449d
Nitin Akash Lakra <42607672+darc-coder@users.noreply.github.com>
2021-06-01 15:28:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
-const mainLinks = document.querySelectorAll(".link");
+let boxb = document.getElementById('boxb');
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("opacity");
-    })
-});
-
-links.forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("open");
-        links.forEach(link => {
-            link.classList.toggle("opacity");
-        })
-    })
-});
+boxb.addEventListener('click', () => {
+    boxb.classList.toggle("active")
+})
